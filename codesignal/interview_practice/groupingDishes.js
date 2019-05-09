f2f1d8652cfba444dc4b50524dd59c601616da2f
Richard Tagashira <rtagashira@gmail.com>
2019-05-08 20:59:15 -0400

    codesignal interview_practice groupingDishes

---
+++ codesignal/interview_practice/groupingDishes.js
@@ -0,0 +1,34 @@
+// You are given a list dishes, where each element consists of a list of strings beginning with the name of the dish, followed by all the ingredients used in preparing it. You want to group the dishes by ingredients, so that for each ingredient you'll be able to find all the dishes that contain it (if there are at least 2 such dishes).
+
+// Return an array where each element is a list beginning with the ingredient name, followed by the names of all the dishes that contain this ingredient. The dishes inside each list should be sorted lexicographically, and the result array should be sorted lexicographically by the names of the ingredients.
+
+
+function groupingDishes(arr) {
+  let obj = {}
+  
+  for(let i=0;i<arr.length;i++){
+    for(let j=1;j<arr[i].length;j++){
+      obj[arr[i][j]]? obj[arr[i][j]].push(arr[i][0]): obj[arr[i][j]] = [arr[i][0]]
+    }
+  }
+  
+  let ingredientsArr = Object.keys(obj)
+  let finalArr = []
+  ingredientsArr.sort()
+  for(let i=0;i<ingredientsArr.length;i++){
+    if(obj[ingredientsArr[i]].length > 1){
+      obj[ingredientsArr[i]].sort()
+      finalArr.push([ingredientsArr[i], ...obj[ingredientsArr[i]]])
+    }
+  }
+  return finalArr
+}
+
+console.log(groupingDishes([["Salad", "Tomato", "Cucumber", "Salad", "Sauce"],
+["Pizza", "Tomato", "Sausage", "Sauce", "Dough"],
+["Quesadilla", "Chicken", "Cheese", "Sauce"],
+["Sandwich", "Salad", "Bread", "Tomato", "Cheese"]]))
+// [["Cheese", "Quesadilla", "Sandwich"],
+// ["Salad", "Salad", "Sandwich"],
+// ["Sauce", "Pizza", "Quesadilla", "Salad"],
+// ["Tomato", "Pizza", "Salad", "Sandwich"]]
