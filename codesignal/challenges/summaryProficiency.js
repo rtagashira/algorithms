4f379504eeb3bc3968d64dda5bfdf469e638120b
Richard Tagashira <rtagashira@gmail.com>
2019-05-12 19:05:31 -0400

    codesignal challenges summaryProficiency

---
+++ codesignal/challenges/summaryProficiency.js
@@ -0,0 +1,15 @@
+// A number of people are applying for a place in some team. Each of them has his own proficiency level (p.l.), which is represented as a positive integer, and the higher the level is - the better.
+
+// There are only n places in the team, and the recruiter hires only those who have their p.l. not less than m.
+
+// All applicants are standing in a queue and if the current one satisfies the required conditions, he is taken into the team right away. Once the team is full, all the remaining people are denied regardless of their p.l.
+
+// What is the total p.l. of the applicants that get be hired? It is guaranteed that all n places can be taken.
+
+function summaryProficiency(a, n, m) {
+  return a.filter(x => x >= m)
+          .slice(0, n)
+          .reduce((prev,next) => prev + next)
+}
+
+console.log(summaryProficiency([4, 2, 3, 6, 2, 5, 4],3,4))//15
