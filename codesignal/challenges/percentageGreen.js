d9aed6df55970a142ad763c10d69d5a3e00aeee8
Richard Tagashira <rtagashira@gmail.com>
2019-05-18 23:41:46 -0400

    codesignal challenges percentageGreen

---
+++ codesignal/challenges/percentageGreen.js
@@ -0,0 +1,7 @@
+// Given the number of green apples on a shelf and total number of apples return the percentage of green apples.
+
+function percentageGreen(green, total) {
+  return green/total * 100
+}
+
+console.log(percentageGreen(2,5))//40
