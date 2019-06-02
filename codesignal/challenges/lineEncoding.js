604e942db34938c3231c4f29659ce418e9489ed5
Richard Tagashira <rtagashira@gmail.com>
2019-06-02 19:12:37 -0400

    csignal challenges lineEncoding

---
+++ codesignal/challenges/lineEncoding.js
@@ -0,0 +1,28 @@
+// Given a string, return its encoding defined as follows:
+
+// First, the string is divided into the least possible number of disjoint substrings consisting of identical characters
+// for example, "aabbbc" is divided into ["aa", "bbb", "c"]
+// Next, each substring with length greater than one is replaced with a concatenation of its length and the repeating character
+// for example, substring "bbb" is replaced by "3b"
+// Finally, all the new strings are concatenated together in the same order and a new string is returned.
+
+function lineEncoding(str) {
+  let newStr = ''
+  let char = str[0]
+  let count = 0
+  for(let i=0;i<str.length;i++){
+    if(char != str[i]){
+      char = str[i]
+      count > 1? newStr += count + str[i-1] : newStr += str[i-1]
+      count = 1
+    }else{
+      count ++
+    }
+    if(i === str.length - 1){
+      count > 1? newStr += count + str[i] : newStr += str[i]
+    }
+  }
+  return newStr
+}
+
+console.log(lineEncoding("aabbbc"))//"2a3bc"
