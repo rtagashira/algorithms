0e9605caea8a8944f0e980057cdbdfa7a093849f
Richard Tagashira <rtagashira@gmail.com>
2019-08-20 19:02:57 -0400

    codesignal challenges findTheRemainder

---
+++ codesignal/challenges/findTheRemainder.js
@@ -0,0 +1,9 @@
+// Given two integers a and b, find the remainder of a when divided by b.
+
+function findTheRemainder(a, b) {
+  return a % b
+}
+
+console.log(findTheRemainder(5,3))//2
+console.log(findTheRemainder(4,2))//0
+console.log(findTheRemainder(3,2))//1
