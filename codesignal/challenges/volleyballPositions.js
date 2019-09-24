cab811682c99dd5e12c11941765e1026c82cb746
Richard Tagashira <rtagashira@gmail.com>
2019-09-24 11:37:38 -0400

    volleyballPositions

--- codesignal/challenges/volleyballPositions.js
+++ codesignal/challenges/volleyballPositions.js
@@ -12,7 +12,9 @@
 // Given the current formation of the team and the number of times k it gained the serve, find the initial position of each player in it.
 
 let volleyballPositions = (arr, k)=> {
-  k = k % 10
+  while(k>6){
+    k -= 6
+  }
   let obj = {}
   let start = [
               [0,3,0],
@@ -20,27 +22,29 @@ let volleyballPositions = (arr, k)=> {
               [0,6,0],
               [5,0,1]
               ]
-  
-  for(let i=0;i<arr.length;i++){
-    for(let j=0;j<arr[0].length;j++){
-      if(start[i][j] === 0){
-        start[i][j] = arr[i][j]
-      }else{
-        let n = start[i][j] + k
-        while(n>6){
-          n -= 6  
+  let pass = 1
+  // first pass puts the player names in the object; second pass fills in the correct player name using the object
+  while(pass <= 2){
+    for(let i=0;i<arr.length;i++){
+      for(let j=0;j<arr[0].length;j++){
+        if(pass === 1){
+          if(start[i][j] === 0){
+            start[i][j] = arr[i][j]
+          }else{
+            let n = start[i][j] + k
+            while(n>6){
+              n -= 6  
+            }
+            obj[n] = arr[i][j] 
+          }
+        }else{
+          if(typeof start[i][j] === 'number'){
+            start[i][j] = obj[start[i][j]]
+          }
         }
-        obj[n] = arr[i][j] 
       }
     }
-  }
-  
-  for(let i=0;i<arr.length;i++){
-    for(let j=0;j<arr[0].length;j++){
-      if(typeof start[i][j] === 'number'){
-        start[i][j] = obj[start[i][j]]
-      }
-    }
+  pass++
   }
   return start
 }
