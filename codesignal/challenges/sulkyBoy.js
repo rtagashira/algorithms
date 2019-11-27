98b9f4b4e25f5f858f424300601a184511e129cc
Richard Tagashira <rtagashira@gmail.com>
2019-11-26 20:58:19 -0500

    codesignal challenges sulkyBoy

---
+++ codesignal/challenges/sulkyBoy.js
@@ -0,0 +1,8 @@
+// Little boy's mother didn't buy him a candy, so now he's sulky and moody. He turns everything you say upside down! If you say that something is true, he'll say it's false, and vice versa.
+
+// Find out what the boy will say about the statement you're about to make.
+
+let sulkyBoy = (statement) => !statement
+
+console.log(sulkyBoy(false))//true
+console.log(sulkyBoy(true))//false
