8f6f3f556074ea734c8580c4d0ae272764bec0e5
Richard Tagashira <rtagashira@gmail.com>
2019-12-29 23:49:08 -0500

    codesignal challenges encloseInBrackets

---
+++ codesignal/challenges/encloseInBrackets.js
@@ -0,0 +1,6 @@
+// Given a string, enclose it in round brackets.
+
+let encloseInBrackets = (str) => '(' + str + ')'
+
+console.log(encloseInBrackets("abcdef"))//"(abcdef)"
+console.log(encloseInBrackets("aaad"))//"(aaad)"
