12b295782b4b9ab7fe93b6db5fadca142b9720f0
Richard Tagashira <rtagashira@gmail.com>
2020-01-31 20:35:51 -0500

    leetcode integerToRoman pass

--- leetcode/integerToRoman.js
+++ leetcode/integerToRoman.js
@@ -18,50 +18,67 @@
 // Given an integer, convert it to a roman numeral. Input is guaranteed to be within the range from 1 to 3999.
 
 const intToRoman = (num) =>{
-    let str = ''
-    while(num > 0){
-        let d = +num.toString()[0]
-        if(num > 1000){
-            num -= 1000
-            str += 'M'
-        }else if(num >= 400){
-            if(d === 9 || d === 4){
-                str += d === 9? 'CM' : 'CD'
-                num -= 100 * d
-            }else if(d >= 5){
-                str += 'D' + 'C'.repeat(d - 5)
-                num -= 100 * d
-            }
-        }else if(num >= 100){
-            str += 'C'.repeat(d)
-            num -= d * 100
-        }else if(num >= 40){
-            if(d === 9 || d === 4){
-                str += d === 9? 'XC' : 'XL'
-                num -= d * 10
-            }else{
-                str += 'L' + 'X'.repeat(d - 5)
-                num -= d * 10
-            }
-        }else if(num >= 10){
-            str += 'X'.repeat(d)
-            num -= d * 10
-        }else if(num >= 4){
-            if(d === 4 || d === 9){
-                str += d === 9? 'IX' : 'IV'
-                num -= d
-            }else{
-                str += 'V' + 'I'.repeat(d - 5)
-                num -= d
-            }
-        }else{
-            str += 'I'.repeat(d)
-            num -= d
+    const s = ['M','CM','D','CD','C','XC','L','XL','X','IX','V','IV','I'],
+          v = [1000,900,500,400,100,90,50,40,10,9,5,4,1]
+    let roman = '',
+        i     = 0
+    while(num>0){
+        if(num >= v[i]){
+            roman += s[i]
+            num -= v[i]
         }
-
+        if(num < v[i]){
+            i++
+        }
     }
-    return str
+    return roman
 }
+
+// const intToRoman = (num) =>{
+//     let str = ''
+//     while(num > 0){
+//         let d = +num.toString()[0]
+//         if(num > 1000){
+//             num -= 1000
+//             str += 'M'
+//         }else if(num >= 400){
+//             if(d === 9 || d === 4){
+//                 str += d === 9? 'CM' : 'CD'
+//                 num -= 100 * d
+//             }else if(d >= 5){
+//                 str += 'D' + 'C'.repeat(d - 5)
+//                 num -= 100 * d
+//             }
+//         }else if(num >= 100){
+//             str += 'C'.repeat(d)
+//             num -= d * 100
+//         }else if(num >= 40){
+//             if(d === 9 || d === 4){
+//                 str += d === 9? 'XC' : 'XL'
+//                 num -= d * 10
+//             }else{
+//                 str += 'L' + 'X'.repeat(d - 5)
+//                 num -= d * 10
+//             }
+//         }else if(num >= 10){
+//             str += 'X'.repeat(d)
+//             num -= d * 10
+//         }else if(num >= 4){
+//             if(d === 4 || d === 9){
+//                 str += d === 9? 'IX' : 'IV'
+//                 num -= d
+//             }else{
+//                 str += 'V' + 'I'.repeat(d - 5)
+//                 num -= d
+//             }
+//         }else{
+//             str += 'I'.repeat(d)
+//             num -= d
+//         }
+
+//     }
+//     return str
+// }
 
 console.log(intToRoman(1994))//"MCMXCIV"
 console.log(intToRoman(100))//"C"
