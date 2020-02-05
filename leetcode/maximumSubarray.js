d49475beb59af560beab499a5296aa67b5a64868
Richard Tagashira <rtagashira@gmail.com>
2020-02-04 23:52:40 -0500

    leetcode maximumSubarray

---
+++ leetcode/maximumSubarray.js
@@ -0,0 +1,17 @@
+// Given an integer array nums, find the contiguous subarray (containing at least one number) which has the largest sum and return its sum.
+
+
+//too slow
+const maxSubArray = (arr) =>{
+    let sum = arr[0]
+    const add = (a)=> a.reduce((a,v)=>a+v)
+    for(let i=0;i<arr.length;i++){
+        for(let j=i+1;j<=arr.length;j++){
+            let temp = add(arr.slice(i,j))
+            if(temp > sum) sum = temp
+        }
+    }
+    return sum
+}
+
+console.log(maxSubArray([-2,1,-3,4,-1,2,1,-5,4]))//6
