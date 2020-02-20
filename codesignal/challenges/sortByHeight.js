c0fd27347dd225ece2455317c1c44305b1c6a176
Richard Tagashira <rtagashira@gmail.com>
2020-02-19 19:46:39 -0500

    codesignal challenges sortByHeight

---
+++ codesignal/challenges/sortByHeight.js
@@ -0,0 +1,14 @@
+// Some people are standing in a row in a park. There are trees between them which cannot be moved. Your task is to rearrange the people by their heights in a non-descending order without moving the trees. People can be very tall!
+
+const sortByHeight = (a) =>{
+    let heights = a.filter(x => x != -1).sort((a,b)=>a-b)
+    for(let i=0;i<a.length;i++){
+        if(a[i] != -1)a[i] = heights.shift()
+    }
+    return a
+}
+
+console.log(sortByHeight([-1, 150, 190, 170, -1, -1, 160, 180]))
+                                                                 //[-1, 150, 160, 170, -1, -1, 180, 190]
+console.log(sortByHeight([23, 54, -1, 43, 1, -1, -1, 77, -1, -1, -1, 3]))
+                                                                          //[1, 3, -1, 23, 43, -1, -1, 54, -1, -1, -1, 77]
