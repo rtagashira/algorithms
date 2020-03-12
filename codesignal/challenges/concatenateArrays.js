8f6beba1f437b5ff43451f753eee00b042637d7c
Richard Tagashira <rtagashira@gmail.com>
2020-03-11 23:38:17 -0400

    codesignal challenges concatenateArrays

---
+++ codesignal/challenges/concatenateArrays.js
@@ -0,0 +1,5 @@
+// Given two arrays of integers a and b, obtain the array formed by the elements of a followed by the elements of b.
+
+const concatenateArrays = (a, b) => [...a,...b]
+
+// const concatenateArrays = (a, b) => a.concat(b)
