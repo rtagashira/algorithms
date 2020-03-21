89e57f0e027dd0d5258ffa236f3a35086e370ab2
Richard Tagashira <rtagashira@gmail.com>
2020-03-21 19:48:34 -0400

    codesignal challenges reverseSentence

---
+++ codesignal/challenges/reverseSentence.js
@@ -0,0 +1,3 @@
+// Reverse the order of words in a given string sentence. You can assume that sentence does not have any leading, trailing or repeating spaces.
+
+const reverseSentence = (s) => s.split(' ').reverse().join(' ')
