2774620d8dc1bdad9384f00ed119e32fb3ad292a
Richard Tagashira <rtagashira@gmail.com>
2020-04-04 21:22:48 -0400

    leetcode repeatedSubstringPattern

---
+++ leetcode/repeatedSubstringPattern.js
@@ -0,0 +1,20 @@
+// Given a non-empty string check if it can be constructed by taking a substring of it and appending multiple copies of the substring together. You may assume the given string consists of lowercase English letters only and its length will not exceed 10000.
+
+const repeatedSubstringPattern = (s) =>{
+    
+    for(let i=1;i<s.length;i++){
+        let str = s.slice(0,i),
+            string = '',
+            count = 0
+        while(string.length < s.length){
+            string += str
+            count++
+        }
+        if(string === s && count >= 2) return true
+    }
+    return false
+}
+
+console.log(repeatedSubstringPattern('abab'))//true
+console.log(repeatedSubstringPattern('aba'))//false
+console.log(repeatedSubstringPattern("abcabcabcabc"))//true
