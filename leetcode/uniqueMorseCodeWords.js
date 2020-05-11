7ae98e4a08d488e2e81d5fd70726fbfcbe481712
Richard Tagashira <rtagashira@gmail.com>
2020-05-11 14:27:29 -0400

    leetcode uniqueMorseCodeWords

---
+++ leetcode/uniqueMorseCodeWords.js
@@ -0,0 +1,23 @@
+// International Morse Code defines a standard encoding where each letter is mapped to a series of dots and dashes, as follows: "a" maps to ".-", "b" maps to "-...", "c" maps to "-.-.", and so on.
+
+// For convenience, the full table for the 26 letters of the English alphabet is given below:
+
+// [".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..","--","-.","---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--.."]
+// Now, given a list of words, each word can be written as a concatenation of the Morse code of each letter. For example, "cba" can be written as "-.-..--...", (which is the concatenation "-.-." + "-..." + ".-"). We'll call such a concatenation, the transformation of a word.
+
+// Return the number of different transformations among all words we have.
+
+const uniqueMorseRepresentations = (words) =>{
+    const alpha = [".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..","--","-.","---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--.."]
+    const morseArr = words.map(x=>{
+        let str = ''
+        for(let letter of x){
+            let charCode = letter.charCodeAt() - 97
+            str += alpha[charCode]
+        }
+        return str
+    })
+    return new Set(morseArr).size
+}
+
+console.log(uniqueMorseRepresentations(["gin", "zen", "gig", "msg"]))//2
