529a1aab25cba5ae565699f831739fa21a92bece
Richard Tagashira <rtagashira@gmail.com>
2020-06-22 15:39:06 -0400

    leetcode maximum69Number

---
+++ leetcode/maximum69Number.js
@@ -0,0 +1,14 @@
+// Given a positive integer num consisting only of digits 6 and 9.
+
+// Return the maximum number you can get by changing at most one digit (6 becomes 9, and 9 becomes 6).
+
+const maximum69Number  = (num) =>{
+    let i = num.toString().indexOf('6')
+    if(i > -1){
+        let arr = [...num.toString()]
+        arr[i] = '9'
+        return +arr.join('')
+    }else{
+        return num
+    }
+}
