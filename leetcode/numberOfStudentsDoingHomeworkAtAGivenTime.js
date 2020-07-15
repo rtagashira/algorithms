d7c750c3312309f0bcf2b9162d3716fb67f2f896
Richard Tagashira <rtagashira@gmail.com>
2020-07-15 15:33:03 -0400

    leetcode numberOfStudentsDoingHomeworkAtAGivenTime

---
+++ leetcode/numberOfStudentsDoingHomeworkAtAGivenTime.js
@@ -0,0 +1,13 @@
+// Given two integer arrays startTime and endTime and given an integer queryTime.
+
+// The ith student started doing their homework at the time startTime[i] and finished it at time endTime[i].
+
+// Return the number of students doing their homework at time queryTime. More formally, return the number of students where queryTime lays in the interval [startTime[i], endTime[i]] inclusive.
+
+const busyStudent = (startTime, endTime, queryTime) =>{
+    let count = 0
+    for(let i=0;i<startTime.length;i++){
+        if(queryTime >= startTime[i] && queryTime <= endTime[i]) count++
+    }
+    return count
+}
