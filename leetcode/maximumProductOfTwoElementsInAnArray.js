a782f74152f22e2b67e4b3ffc6139e34d0ce7b69
Richard Tagashira <rtagashira@gmail.com>
2020-07-18 15:43:57 -0400

    leetcode maximumProductOfTwoElementsInAnArray

---
+++ leetcode/maximumProductOfTwoElementsInAnArray.js
@@ -0,0 +1,6 @@
+// Given the array of integers nums, you will choose two different indices i and j of that array. Return the maximum value of (nums[i]-1)*(nums[j]-1).
+
+const maxProduct = (nums) =>{
+    nums.sort((a,b)=>b-a)
+    return (nums[0]-1) * (nums[1]-1)
+}
