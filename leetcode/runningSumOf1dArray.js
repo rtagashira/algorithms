5e5cfb8eff3713679127d9b459fb17bf5244999f
Richard Tagashira <rtagashira@gmail.com>
2020-07-22 14:37:50 -0400

    leetcode runningSumOf1dArray

---
+++ leetcode/runningSumOf1dArray.js
@@ -0,0 +1,10 @@
+// Given an array nums. We define a running sum of an array as runningSum[i] = sum(nums[0]…nums[i]).
+
+// Return the running sum of nums.
+
+const runningSum = (nums) =>{
+    for(let i=1;i<nums.length;i++){
+        nums[i] = nums[i] + nums[i-1]
+    }
+    return nums
+}
