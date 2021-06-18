eac2f7f44c339518dd0c41839b77bed23f24dbb3
Richard Tagashira <rtagashira@gmail.com>
2021-06-18 13:30:22 -0400

    leetcode arithmeticSubarrays

---
+++ leetcode/arithmeticSubarrays.js
@@ -0,0 +1,34 @@
+// A sequence of numbers is called arithmetic if it consists of at least two elements, and the difference between every two consecutive elements is the same. More formally, a sequence s is arithmetic if and only if s[i+1] - s[i] == s[1] - s[0] for all valid i.
+
+// For example, these are arithmetic sequences:
+
+// 1, 3, 5, 7, 9
+// 7, 7, 7, 7
+// 3, -1, -5, -9
+// The following sequence is not arithmetic:
+
+// 1, 1, 2, 5, 7
+// You are given an array of n integers, nums, and two arrays of m integers each, l and r, representing the m range queries, where the ith query is the range [l[i], r[i]]. All the arrays are 0-indexed.
+
+// Return a list of boolean elements answer, where answer[i] is true if the subarray nums[l[i]], nums[l[i]+1], ... , nums[r[i]] can be rearranged to form an arithmetic sequence, and false otherwise.
+
+const checkArithmeticSubarrays = (nums, l, r) =>{
+    let arr = []
+    for(let i=0;i<l.length;i++){
+        let a = nums.slice(l[i],r[i] + 1)
+        a.sort((a,b)=>a-b)
+        let d = a[1] - a[0]
+        let bool = true
+        for(let j=1;j<a.length;j++){
+            if(a[j] - a[j-1] !== d){
+                arr.push(false)
+                bool = false
+                break
+            }
+        }
+        if(bool) arr.push(true)
+    }
+    return arr
+}
+
+console.log(checkArithmeticSubarrays(nums = [4,6,5,9,3,7], l = [0,0,2], r = [2,3,5]))//[true,false,true]
