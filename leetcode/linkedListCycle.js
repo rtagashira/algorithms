ce5695665719afe294d025b19ecbbc9be2dcfede
Richard Tagashira <rtagashira@gmail.com>
2021-10-08 11:57:31 -0400

    leetcode linkedListCycle

---
+++ leetcode/linkedListCycle.js
@@ -0,0 +1,18 @@
+// Given head, the head of a linked list, determine if the linked list has a cycle in it.
+
+// There is a cycle in a linked list if there is some node in the list that can be reached again by continuously following the next pointer. Internally, pos is used to denote the index of the node that tail's next pointer is connected to. Note that pos is not passed as a parameter.
+
+// Return true if there is a cycle in the linked list. Otherwise, return false.
+
+const hasCycle = (head) =>{
+    let slow = head
+    let fast = head
+    
+    while(slow){
+        if(slow) slow = slow.next
+        if(fast) fast = fast.next
+        if(fast) fast = fast.next
+        if(slow === fast && fast !== null) return true
+    }
+    return false
+}
