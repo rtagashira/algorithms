5bfa0d87711aa9ea0bb4a83390ad6a597eb1d193
Richard Tagashira <rtagashira@gmail.com>
2021-12-19 21:31:37 -0500

    leetcode findFirstPalindromicStringInTheArray

---
+++ leetcode/findFirstPalindromicStringInTheArray.js
@@ -0,0 +1,10 @@
+// Given an array of strings words, return the first palindromic string in the array. If there is no such string, return an empty string "".
+
+// A string is palindromic if it reads the same forward and backward.
+
+const firstPalindrome = (words) =>{
+    for(let word of words){
+        if(word === [...word].reverse().join('')) return word
+    }
+    return ''
+}
